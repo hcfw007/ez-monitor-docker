53d7e23eec6f94f461fd6f2c212b133cf825c247
NickWang <brotherstyx@gmail.com>
2021-12-20 11:16:00 +0800

    feat: 🎸 console docker

---
+++ services/xprofiler-console/config.local.js
@@ -0,0 +1,41 @@
+'use strict';
+module.exports = () => {
+  const config = {};
+
+  config.mysql = {
+    app: true,
+    agent: false,
+    clients: {
+      xprofiler_console: {
+        host: process.env.MYSQL_HOST,
+        port: process.env.MYSQL_PORT,
+        user: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PASSWORD,
+        database: 'xprofiler_console',
+      },
+      xprofiler_logs: {
+        host: process.env.MYSQL_HOST,
+        port: process.env.MYSQL_PORT,
+        user: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PASSWORD,
+        database: 'xprofiler_logs',
+      },
+    },
+  };
+
+  config.redis = {
+    client: {
+      sentinels: null,
+      port: process.env.REDIS_PORT,
+      host: process.env.REDIS_HOST,
+      password: process.env.REDIS_PASSWORD,
+      db: 0,
+    },
+  };
+
+  config.xprofilerConsole = process.env.CONSOLE_ENDPOINT;
+
+  config.xtransitManager = process.env.MANAGER_ENDPOINT;
+
+  return config;
+};
